8436a6bc517d35cb250a3efa86e19e2fef024d7f
TongQuangThanh <tongquangthanh94@gmail.com>
2022-05-22 00:02:50 +0700

    init log feature

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -19,16 +19,6 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 @app.route('/home')
 @login_required
 def home():
-    log = request.values.get('log_button')
-    if log:
-      if log == 'info':
-        app.logger.info('No issue')
-      elif log == 'warning':
-        app.logger.warning('It a warning')
-      elif log == 'error':
-        app.logger.error('Error!!!')
-      elif log == 'critical':
-        app.logger.critical('Critical thing occurred!!!')
     user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
     return render_template(
@@ -77,11 +67,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.error('Invalid login attempt!!!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+        app.logger.info('admin logged in successfully')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
